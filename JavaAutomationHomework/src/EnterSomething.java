51666f9d17989ead09a3906fdb6585d4a2331eeb
Kristina Stasilevich <kristina.stasilevich@gmail.com>
2024-03-11 21:53:46 +0300

    Changed array and cycle

--- JavaAutomationHomework/src/EnterSomething.java
+++ JavaAutomationHomework/src/EnterSomething.java
@@ -5,17 +5,10 @@ public class EnterSomething {
         String str = new String();
         Scanner scanner = new Scanner(System.in);
         String exitFromCycle = "Exit";
-        while (true) {
-            if(str.length() == 0) {
-                System.out.print("Please enter something ");
-                str = scanner.nextLine();
-            } else if(!str.isEmpty() & !str.equals(exitFromCycle)) {
-                System.out.print("Please enter something ");
-                str = scanner.nextLine();
-            } else {
-                break;
-            }
-        }
-
+        do {
+            System.out.print("Please enter something ");
+            str = scanner.nextLine();
+        } while (!str.equals(exitFromCycle));
     }
 }
+
--- JavaAutomationHomework/src/RemoteController.java
+++ JavaAutomationHomework/src/RemoteController.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class RemoteController {
     public static void main(String[] args) {
-        String[] channels = new String[10];
-        channels[0] = "";
+        String[] channels = new String[9];
+        channels[0] = "Охота и рыбалка";
         channels[1] = "Cinema";
         channels[2] = "Комедия";
         channels[3] = "Телепутешествия";
@@ -12,19 +12,16 @@ public class RemoteController {
         channels[6] = "Карусель";
         channels[7] = "Дорама";
         channels[8] = "Мульт";
-        channels[9] = "Охота и рыбалка";
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Please, enter channel number ");
             int channelNumber = scanner.nextInt();
             if (channelNumber != 0) {
-                System.out.println(channels[channelNumber]);
+                System.out.println(channels[channelNumber - 1]);
             } else {
                 break;
             }
-
         }
-
     }
 }
